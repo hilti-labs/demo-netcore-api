0b2973bb078a2dd430dd70fa696e66b46a975a5f
Anthony Sneed <anthonysneed@Anthonys-MacBook-Pro.local>
2018-07-23 09:17:28 -0500

    Add value.

--- src/Controllers/ValuesController.cs
+++ src/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace demo_netcore_2x_api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var result = new [] { "value1", "value2", "value3" };
+            var result = new [] { "value1", "value2", "value3", "value4" };
             return Ok(result);
         }
 
